7c60039c96f3ed6d080fdc3c4cdd6d2632ab45c5
EnterTheVortex <dan.j_90@hotmail.co.uk>
2025-09-07 13:54:05 +0100

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,22 @@
+// Example question set (expand later)
+const questions = [
+  {
+    question: "What does a red traffic light mean?",
+    options: ["Stop", "Go", "Slow down"],
+    answer: 0
+  },
+  {
+    question: "What shape is a stop sign?",
+    options: ["Circle", "Octagon", "Triangle"],
+    answer: 1
+  },
+  {
+    question: "When should you use hazard lights?",
+    options: [
+      "When driving normally",
+      "When your car is stationary and causing an obstruction",
+      "On the motorway at all times"
+    ],
+    answer: 1
+  }
+];
