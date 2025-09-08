ec8cbc82f1c08fafd981a9e6cecb98348c59a89d
EnterTheVortex <dan.j_90@hotmail.co.uk>
2025-09-08 23:14:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -528,3 +528,16 @@ function showHazardSummary() {
     </div>
   `;
 }
+
+// ----------------- MOBILE NAVIGATION -----------------
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.querySelector(".hamburger");
+  const nav = document.querySelector(".header-nav");
+
+  if (hamburger && nav) {
+    hamburger.addEventListener("click", () => {
+      nav.classList.toggle("show");
+    });
+  }
+});
+
